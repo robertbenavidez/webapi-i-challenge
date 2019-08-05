11240d717d384b22967ba76f8509fe3ec3236000
Robert Benavidez <murabitbenavidez@gmail.com>
2019-08-05 16:00:21 -0400

    initial commit server setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+
+
+server.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+
+const port = 8000;
+server.listen(port, () => console.log('api running'));
