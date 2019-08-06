fca8edbc3f22abb2ea079e5d03df65354218c7eb
Robert Benavidez <murabitbenavidez@gmail.com>
2019-08-05 22:29:34 -0400

    POST request

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 
+const Users = require('./data/db');
+
 const server = express();
 
+server.use(express.json())
 
-server.get('/', (req, res) => {
-    res.send('hello world')
-})
+
+server.post('/api/users', (req, res) => {
+    const {name, bio} = req.body;
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+    } else {
+        Users.insert(req.body)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(() => {
+            res.status(500).json({errorMessage: 'Error adding user.'});
+        });
+    }
+});
+
+
+
+
+
+
 
 
 const port = 8000;
