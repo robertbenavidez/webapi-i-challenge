ea9586d982dfadc159c8871c9d930e6e390e022a
Robert Benavidez <murabitbenavidez@gmail.com>
2019-08-05 23:06:36 -0400

    GET request to retrieve user by ID

--- index.js
+++ index.js
@@ -33,6 +33,20 @@ server.get('/api/users', (req, res) => {
 
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const userID = req.params.id;
+    Users.findById(userID)
+    .then(user => {
+        if (user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+    });
+});
 
 
 
