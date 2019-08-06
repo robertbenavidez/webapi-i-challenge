a95b518fa9f41adb7c26a4517d7e2640ddecfa83
Robert Benavidez <murabitbenavidez@gmail.com>
2019-08-05 23:53:07 -0400

    mvp complete

--- index.js
+++ index.js
@@ -63,6 +63,25 @@ server.delete('/api/users/:id', (req, res) => {
     });
 });
 
+server.put('/api/users/:id', (req, res) => {
+    const userID = req.params.id;
+    const {name, bio} = req.body;
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."});
+    } else {
+        Users.update(userID, req.body)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ errorMessage: "The user information could not be modified." });
+        });
+    }
+});
 
 
 
